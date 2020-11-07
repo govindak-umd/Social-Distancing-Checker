67477deae5207a1e372b712c17d5d1762139ae22
Govind <govindajithkumar@gmail.com>
2020-11-07 15:35:02 -0500

    added scripts before linking weights and adding an initial result video

--- TensorFlow-2.x-YOLOv3/detection_custom.py
+++ TensorFlow-2.x-YOLOv3/detection_custom.py
@@ -17,12 +17,20 @@ from yolov3.yolov3 import Create_Yolov3
 from yolov3.utils import load_yolo_weights, detect_image, detect_video, detect_realtime
 from yolov3.configs import *
 import time
+
+if tf.test.gpu_device_name(): 
+
+    print('-------->>Default GPU Device:{}'.format(tf.test.gpu_device_name()))
+
+else:
+
+   print("Please install GPU version of TF")
 input_size = YOLO_INPUT_SIZE
 Darknet_weights = YOLO_DARKNET_WEIGHTS
 # if TRAIN_YOLO_TINY:
 #     Darknet_weights = YOLO_DARKNET_TINY_WEIGHTS
 time_s = time.time()
-video_path = ["Test_Video/people_walking.mp4"]
+video_path = ["Test_Video/people_walking_3.mp4"]
 # image_path = ["test_images_and_videos/deer_cow.jpg",
 # 			"test_images_and_videos/84353914_188324fbb9_b.jpg",
 # 			"test_images_and_videos/87180235_d8bb660c55_b.jpg",
@@ -35,7 +43,7 @@ count=0
 # 	detect_image(yolo, i, "./IMAGES/new_image_"+str(count)+".jpg", input_size=input_size, show=True, CLASSES=TRAIN_CLASSES, rectangle_colors=(255,0,0))
 # 	count+=1
 for v in video_path:
-	detect_video(yolo, v, "./IMAGES/new_image_"+str(count)+".mp4", input_size=input_size, show=False, CLASSES=TRAIN_CLASSES, rectangle_colors=(255,0,0))
+	detect_video(yolo, v, "./IMAGES/Result_CCTV_Video"+str(count)+".mp4", input_size=input_size, show=False, CLASSES=TRAIN_CLASSES, rectangle_colors=(255,0,0))
 	count+=1
 time_end = time.time()
 time_total = time_end - time_s
